98e3e3f298a8faaea1de33c533afdc628e894ed3
Ravula Koushal Sai <149877318+koushalsai@users.noreply.github.com>
2025-07-15 23:41:56 +0530

    Refactor layout and homepage, update dependencies

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,14 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import localFont from "next/font/local";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -22,15 +23,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > <>  
-        <Toaster  position='top-right' richColors expand={true}/>
-        </>
-        {children}
-      </body>
-    </html>
+      <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+        <body className="font-sans antialiased">
+          <Toaster position="top-right" richColors expand />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
